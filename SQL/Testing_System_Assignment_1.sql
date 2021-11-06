d9f18f0ab4f2da6fc9a1a64ffb42ad559bffc8cb
hiep1006 <anhhiep1006@gmail.com>
2021-11-06 21:53:21 +0700

    Assignment 1 SQL

--- SQL/Testing_System_Assignment_1.sql
+++ SQL/Testing_System_Assignment_1.sql
@@ -3,36 +3,36 @@ CREATE DATABASE Testing_System_Assignment_1;
 USE Testing_System_Assignment_1;
 
 CREATE TABLE Department(
-	DepartmentID tinyint unsigned,
-    DepartmentName varchar(50)
+	DepartmentID 		tinyint unsigned,
+    DepartmentName 		varchar(50)
 );
 
 CREATE TABLE `Position`(
-	PositionID tinyint unsigned,
-    PositionName varchar(50)
+	PositionID 			tinyint unsigned,
+    PositionName 		varchar(50)
 );
 
 CREATE TABLE `Account`(
-	AccountID int unsigned,
-    Email varchar(50),
-    Username varchar(50),
-    FullName varchar(50),
-    DepartmentID tinyint unsigned,
-    PositionID tinyint unsigned,
-    CreateDate date
+	AccountID 			int unsigned,
+    Email 				varchar(50),
+    Username 			varchar(50),
+    FullName 			varchar(50),
+    DepartmentID 		tinyint unsigned,
+    PositionID 			tinyint unsigned,
+    CreateDate 			date
 );
 
 CREATE TABLE `Group`(
-	GroupID tinyint unsigned,
-    GroupName varchar(50),
-    CreatorID int unsigned,
-    CreateDate date
+	GroupID 			tinyint unsigned,
+    GroupName 			varchar(50),
+    CreatorID 			int unsigned,
+    CreateDate 			date
 );
 
 CREATE TABLE GroupAccount(
-	GroupID tinyint unsigned,
-    AccountID int unsigned,
-    JoinDate date
+	GroupID 			tinyint unsigned,
+    AccountID 			int unsigned,
+    JoinDate 			date
 );
 
 CREATE TABLE TypeQuestion(
@@ -41,17 +41,17 @@ CREATE TABLE TypeQuestion(
 );
 
 CREATE TABLE CategoryQuestion(
-	CategoryID tinyint unsigned,
-    CategoryName varchar(50)
+	CategoryID 			tinyint unsigned,
+    CategoryName 		varchar(50)
 );
 
 CREATE TABLE Question(
-	QuestionID tinyint unsigned,
-    Content varchar(50),
-    CategoryID tinyint unsigned,
-    TypeID tinyint unsigned,
-    CreatorID int unsigned,
-    CreateDate date
+	QuestionID 			tinyint unsigned,
+    Content 			varchar(50),
+    CategoryID 			tinyint unsigned,
+    TypeID 				tinyint unsigned,
+    CreatorID 			int unsigned,
+    CreateDate 			date
 );
 
 CREATE TABLE Answer(
@@ -62,16 +62,16 @@ CREATE TABLE Answer(
 );
 
 CREATE TABLE Exam(
-	ExamID tinyint unsigned,
-    `Code` tinyint unsigned,
-    Title varchar(50),
-    CategoryID tinyint unsigned,
-    Duration tinyint unsigned,
-    CreatorID tinyint unsigned,
-    CreateDate date
+	ExamID 				tinyint unsigned,
+    `Code` 				tinyint unsigned,
+    Title 				varchar(50),
+    CategoryID 			tinyint unsigned,
+    Duration 			tinyint unsigned,
+    CreatorID 			tinyint unsigned,
+    CreateDate 			date
 );
 
 CREATE TABLE ExamQuestion(
-	ExamID TINYINT unsigned,
-    QuestionID tinyint unsigned
+	ExamID 				TINYINT unsigned,
+    QuestionID 			tinyint unsigned
 );
