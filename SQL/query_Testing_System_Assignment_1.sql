290e53754a7e14892eb60f0a7cb4f0c61d5eaa77
hiep1006 <anhhiep1006@gmail.com>
2021-11-07 20:23:53 +0700

    cap nhat bai 1

--- SQL/query_Testing_System_Assignment_1.sql
+++ SQL/query_Testing_System_Assignment_1.sql
@@ -10,16 +10,19 @@ VALUE	("Truong phong"),
         ("Nhan vien");
         
 INSERT INTO `Account`(Email, Username, FullName, DepartmentID, PositionID, CreateDate)
-VALUES	("truongphongHR@123.com" , "truongphongHR" , "Nguyen Van A" , 1 , 1, "2021-11-7"),
-		( "PhoPhongHR@123.com" , "PhoPhongHR" , "Nguyen Van B" , 1 , 2 , "2021-11-7"),
-        ( "NhanVienHR@123.com" , "NhanVienHR", "Nguyen Van C" , 1 , 3 , "2021-11-7"),
-        ( "TruongPhongSALE@123.com" , "TruongPhongSALE", "Nguyen Van D" , 2 , 1 , "2021-11-7"),
-        ( "PhoPhongSALE@123.com" , "PhoPhongSALE", "Nguyen Van E" , 2 , 2 , "2021-11-7"),
+VALUES	("truongphongHR@123.com" , "truongphongHR" , "Ngu" , 1 , 1, "2021-11-7"),
+		( "PhoPhongHR@123.com" , "PhoPhongHR" , "Nguyen Van B1" , 1 , 2 , "2021-11-7"),
+        ( "NhanVienHR@123.com" , "NhanVienHR", "Nguyen Van C12" , 1 , 3 , "2021-11-7"),
+        ( "TruongPhongSALE@123.com" , "TruongPhongSALE", "Nguyen Van D123" , 2 , 1 , "2021-11-7"),
+        ( "PhoPhongSALE@123.com" , "PhoPhongSALE", "Nguyen Van 1234" , 2 , 2 , "2021-11-7"),
         ( "NhanVienSale@123.com" , "NhanVienSale", "Nguyen Van F" , 2 , 3 , "2021-11-7"),
         ( "TruongPhongMarketing@123.com" , "TruongPhongMarketing" , "Nguyen Van G" , 3 , 1 , "2021-11-7"),
         ( "PhoPhongMarketing@123.com" , "PhoPhongMarketing" , "Nguyen Van H" , 3 , 2 , "2021-11-7"),
         ( "NhanVienMarketing@123.com" , "NhanVienMarketing" , "Nguyen Van I" , 3 , 3 , "2021-11-7");
+
 DELETE FROM `Account` WHERE AccountID = 1;
 SELECT * FROM Department;
+SELECT DepartmentID FROM Department WHERE DepartmentName = "SALE";
 SELECT * FROM `Position`;
 SELECT * FROM `Account`;
+SELECT * FROM `Account` WHERE (length(FullName)) = (SELECT MAX(length(FullName)) FROM `Account`);
