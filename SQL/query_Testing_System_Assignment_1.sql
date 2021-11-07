405b2938f6a1903ac0459c6ac7c12a11e84c39c5
hiep1006 <anhhiep1006@gmail.com>
2021-11-07 15:37:44 +0700

    cap nhat bai 1

--- SQL/query_Testing_System_Assignment_1.sql
+++ SQL/query_Testing_System_Assignment_1.sql
@@ -19,6 +19,7 @@ VALUES	("truongphongHR@123.com" , "truongphongHR" , "Nguyen Van A" , 1 , 1, "202
         ( "TruongPhongMarketing@123.com" , "TruongPhongMarketing" , "Nguyen Van G" , 3 , 1 , "2021-11-7"),
         ( "PhoPhongMarketing@123.com" , "PhoPhongMarketing" , "Nguyen Van H" , 3 , 2 , "2021-11-7"),
         ( "NhanVienMarketing@123.com" , "NhanVienMarketing" , "Nguyen Van I" , 3 , 3 , "2021-11-7");
+DELETE FROM `Account` WHERE AccountID = 1;
 SELECT * FROM Department;
 SELECT * FROM `Position`;
 SELECT * FROM `Account`;
