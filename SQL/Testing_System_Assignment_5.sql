19b6f783b9b9e5be73a94eb8bd361ccdec707ee0
hiep1006 <anhhiep1006@gmail.com>
2021-11-18 19:40:13 +0700

    sua bai 5

--- SQL/Testing_System_Assignment_5.sql
+++ SQL/Testing_System_Assignment_5.sql
@@ -3,14 +3,8 @@
 CREATE VIEW NhanVien_Sale AS
 SELECT `Account`.*, `Position`.PositionName , Department.DepartmentName
 FROM `Account` , `Position` , Department
-WHERE `Account`.PositionID = `Position`.PositionID AND `Account`.DepartmentID = Department.DepartmentID AND departmentName = 
-(
-	SELECT departmentName FROM department WHERE departmentName = "Phòng kinh doanh"
-)
-AND positionName = 
-(
-	SELECT PositionName FROM `Position` WHERE PositionName = "Nhân viên"
-) ;
+WHERE `Account`.PositionID = `Position`.PositionID AND `Account`.DepartmentID = Department.DepartmentID AND departmentName = "Phòng kinh doanh" AND positionName = "Nhân viên";
+
 
 -- 2
 CREATE VIEW Max_Gr AS
@@ -21,23 +15,23 @@ GROUP BY GroupAccount.AccountID HAVING count(GroupAccount.GroupID) = (
 	SELECT max(MAX_GR) FROM (SELECT count(DISTINCT GroupAccount.GroupID) AS MAX_GR FROM GroupAccount GROUP BY GroupAccount.AccountID) AS abc
 );
 
+
+
 -- 3
 CREATE VIEW view_1 AS
 SELECT Content , length(Content)
 FROM Question
 WHERE length(Content) > 300;
-DELETE FROM view_1;
+
+
 
 -- 4
 CREATE VIEW view_2 AS
-SELECT Department.DepartmentName , count(AccountID) 
+SELECT Department.DepartmentName , count(AccountID)
 FROM `Account`
-JOIN `Position` ON `Account`.PositionID = `Position`.PositionID  AND `Position`.PositionName = (
-	SELECT PositionName FROM `Position` WHERE PositionName = "Nhân viên"
-)
+JOIN `Position` ON `Account`.PositionID = `Position`.PositionID  AND `Position`.PositionName = "Nhân viên"
 JOIN Department ON `Account`.DepartmentID = Department.DepartmentID
-GROUP BY `Account`.DepartmentID;
-
+GROUP BY `Account`.DepartmentID HAVING  count(AccountID) = (SELECT max(_max) FROM (SELECT count(AccountID) AS _max FROM `Account` WHERE `Account`.PositionID = 13 GROUP BY `Account`.DepartmentID) AS abc);
 
 -- 5
 CREATE VIEW view_3 AS
